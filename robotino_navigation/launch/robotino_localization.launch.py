964d6c9a6ebbdc9fc24f3438ee24f06fe0181307
srb <saurabh.borse@alumni.fh-aachen.de>
2024-01-12 16:41:34 +0100

    Created new package for nav2_stack

---
+++ robotino_navigation/launch/robotino_localization.launch.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, LogInfo, OpaqueFunction, GroupAction
+from launch.substitutions import LaunchConfiguration, TextSubstitution, PythonExpression
+from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
+from launch.conditions import IfCondition
+from launch_ros.descriptions import ComposableNode, ParameterFile
+import launch
+from launch_ros.actions import LoadComposableNodes
+
+def launch_nodes_withconfig(context, *args, **kwargs):
+
+    bringup_dir = get_package_share_directory('robotino3_navigation')
+    
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration('namespace')
+    map_yaml_file = LaunchConfiguration('map')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    autostart = LaunchConfiguration('autostart')
+    params_file = LaunchConfiguration('params_file')
+    use_respawn = LaunchConfiguration('use_respawn')
+    log_level = LaunchConfiguration('log_level')
+    launch_rviz = LaunchConfiguration('launch_rviz')
+    launch_mapserver = LaunchConfiguration('launch_mapserver')
+    
+    lifecycle_nodes = ['map_server', 'amcl']
+    
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {
+        'use_sim_time': use_sim_time,
+        'yaml_filename': map_yaml_file}
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True),
+        allow_substs=True)
+    
+    launch_configuration = {}
+    for argname, argval in context.launch_configurations.items():
+        launch_configuration[argname] = argval
+    
+    # Create the remappings for the nodes
+    remappings = [('/'+launch_configuration['namespace']+'/tf', '/tf'),
+                  ('/'+launch_configuration['namespace']+'/tf_static', '/tf_static'),
+                  ('/'+launch_configuration['namespace']+'/map','/map')]
+    
+    rviz_config_dir = os.path.join(bringup_dir,'rviz', 'robotino_localization.rviz')
+    
+    # Create list of nodes to launch
+    load_nodes = GroupAction(
+        actions=[
+            Node(
+                package='nav2_map_server',
+                executable='map_server',
+                name='map_server',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings,
+                condition=IfCondition(launch_mapserver),
+                namespace=namespace),
+            Node(
+                package='nav2_amcl',
+                executable='amcl',
+                name='amcl',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings,
+                namespace=namespace),
+            Node(
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager_localization',
+                output='screen',
+                arguments=['--ros-args', '--log-level', log_level],
+                parameters=[{'use_sim_time': use_sim_time},
+                            {'autostart': autostart},
+                            {'node_names': lifecycle_nodes}],
+                namespace=namespace), 
+        ])
+    
+    return[load_nodes]
+
+def generate_launch_description():
+    
+    bringup_dir = get_package_share_directory('robotino3_navigation')
+
+    # Declare the launch arguments
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        'RCUTILS_LOGGING_BUFFERED_STREAM', '1')
+    
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace', default_value='',
+        description='Top-level namespace')
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',default_value=os.path.join(bringup_dir, 'map', 'map.yaml'),
+        description='Full path to map yaml file to load')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time', default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the nav2 stack')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(bringup_dir, 'config', 'robotinobase3_nav2_params.yaml'),
+        description='Full path to the ROS2 parameters file to use')
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        'use_respawn', default_value='False',
+        description='Whether to respawn if a node crashes. Applied when composition is disabled.')
+
+    declare_log_level_cmd = DeclareLaunchArgument(
+        'log_level', default_value='info',
+        description='log level')
+    
+    launch_rviz_argument = DeclareLaunchArgument(
+        'launch_rviz',
+        default_value='false', 
+        description= 'Wheather to start Rvizor not based on launch environment')
+    
+    launch_mapserver_argument = DeclareLaunchArgument(
+        'launch_mapserver',
+        default_value='false', 
+        description= 'Wheather to launch map server or not')
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Set environment variables
+    ld.add_action(stdout_linebuf_envvar)
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_log_level_cmd)
+    ld.add_action(launch_rviz_argument)
+    ld.add_action(launch_mapserver_argument)
+
+    # Add the actions to launch all of the localiztion nodes
+    ld.add_action(OpaqueFunction(function=launch_nodes_withconfig))
+
+    return ld
---
+++ robotino_navigation/launch/robotino_rviz.launch.py
@@ -0,0 +1,96 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler, OpaqueFunction
+from launch.conditions import IfCondition, UnlessCondition
+from launch.event_handlers import OnProcessExit
+from launch.events import Shutdown
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from nav2_common.launch import ReplaceString
+
+def launch_nodes_withconfig(context, *args, **kwargs):
+    # Get the launch directory
+    bringup_dir = get_package_share_directory('robotino3_navigation')
+    
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
+    rviz_config_file = LaunchConfiguration('rviz_config')
+
+    launch_configuration = {}
+    for argname, argval in context.launch_configurations.items():
+        launch_configuration[argname] = argval
+        
+    rviz_config_dir = os.path.join(bringup_dir,'rviz', launch_configuration['rviz_config'])
+    
+    start_namespaced_rviz_cmd = Node(
+        condition=IfCondition(use_namespace),
+        package='rviz2',
+        executable='rviz2',
+        namespace=namespace,
+        arguments=['-d', rviz_config_dir],
+        output='screen',
+        remappings=[('/'+launch_configuration['namespace']+'/map', '/map'),
+                    ('/'+launch_configuration['namespace']+'/tf', '/tf'),
+                    ('/'+launch_configuration['namespace']+'/tf_static', 'tf_static'),
+                    ('/goal_pose', '/'+launch_configuration['namespace']+'/goal_pose'),
+                    ('/clicked_point', '/'+launch_configuration['namespace']+'/clicked_point'),
+                    ('/initialpose', '/'+launch_configuration['namespace']+'/initialpose')])
+
+    exit_event_handler_namespaced = RegisterEventHandler(
+        condition=IfCondition(use_namespace),
+        event_handler=OnProcessExit(
+            target_action=start_namespaced_rviz_cmd,
+            on_exit=EmitEvent(event=Shutdown(reason='rviz exited'))))
+        
+    return [start_namespaced_rviz_cmd, 
+            exit_event_handler_namespaced]
+
+def generate_launch_description():
+    
+    bringup_dir = get_package_share_directory('robotino3_navigation')
+   
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description=('Top-level namespace. The value will be used to replace the '
+                     '<robot_namespace> keyword on the rviz config file.'))
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='true',
+        description='Whether to apply a namespace to the navigation stack')
+
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        'rviz_config',
+        default_value=os.path.join(bringup_dir, 'rviz', 'robotinobase3_nav2config.rviz'),
+        description='Full path to the RVIZ config file to use')
+
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_namespace_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+
+    ld.add_action(OpaqueFunction(function=launch_nodes_withconfig))
+    
+    return ld
