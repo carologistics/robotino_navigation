1e1964cb6754cacb6cb5d17c7f82d5222d6f70ab
Tim Wendt <tim.wendt@alumni.fh-aachen.de>
2024-04-13 23:11:01 +0200

    motor_move: started working

---
+++ motor_move/src/motor_move.cpp
@@ -0,0 +1,52 @@
+#include "motor_move/motor_move.hpp"
+
+namespace motor_move {
+    MotorMove::MotorMove(const rclcpp::NodeOptions & options) : Node("motor_move", options) {
+       cmd_vel_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+       using namespace std::placeholders;
+       action_server_ = rclcpp_action::create_server<MotorMoveAction>(
+           this,
+            "motor_move_action",
+           std::bind(&MotorMove::handle_goal, this, _1, _2),
+           std::bind(&MotorMove::handle_cancel, this, _1),
+           std::bind(&MotorMove::handle_accepted, this, _1)
+       );
+
+       tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+       tf_listener_ = std::make_unique<tf2_ros::TransformListener>(*tf_buffer_);
+    }
+    MotorMove::~MotorMove() {
+    }
+    rclcpp_action::GoalResponse MotorMove::handle_goal(
+        const rclcpp_action::GoalUUID & uuid,
+        std::shared_ptr<const MotorMoveAction::Goal> goal) {
+        (void)uuid;
+        RCLCPP_INFO(this->get_logger(), "Received goal requst with order %f", goal->goal.position.x);
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
+
+    rclcpp_action::CancelResponse MotorMove::handle_cancel(
+        const std::shared_ptr<GoalHandleMotorMove> goal_handle) {
+        (void)goal_handle;
+        RCLCPP_INFO(this->get_logger(), "Received request to cancle goal");
+        return rclcpp_action::CancelResponse::ACCEPT;
+    }
+
+    void MotorMove::handle_accepted(const std::shared_ptr<GoalHandleMotorMove> goal_handle) {
+        using namespace std::placeholders;
+        std::thread{std::bind(&MotorMove::execute, this, _1), goal_handle}.detach();
+    }
+
+    void MotorMove::execute(const std::shared_ptr<GoalHandleMotorMove> goal_handle) {
+        (void)goal_handle;
+        rclcpp::Rate loop_rate(15);
+        RCLCPP_INFO(this->get_logger(), "Execute goal");
+        loop_rate.sleep();
+    }
+}
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<motor_move::MotorMove>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+}
