c70828c75a376d4a38afdbfd46be3ea8d425dc90
Tim Wendt <tim.wendt@alumni.fh-aachen.de>
2024-04-14 21:53:59 +0200

    motor_move: first delta calculation

--- motor_move/include/motor_move/motor_move.hpp
+++ motor_move/include/motor_move/motor_move.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
-#include "motor_move_msgs/action/motor_move.hpp"
-#include "geometry_msgs/msg/twist.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <motor_move_msgs/action/motor_move.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 namespace motor_move {
 using MotorMoveAction = motor_move_msgs::action::MotorMove;
 using GoalHandleMotorMove = rclcpp_action::ServerGoalHandle<MotorMoveAction>;
+using PoseStamped = geometry_msgs::msg::PoseStamped;
+using TransformStamped = geometry_msgs::msg::TransformStamped;
+using Pose = geometry_msgs::msg::Pose;
 class MotorMove : public rclcpp::Node {
 public:
     explicit MotorMove(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
@@ -21,6 +27,10 @@ private:
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
+    std::mutex target_pose_mutex_;
+    PoseStamped target_pose_;
+
+    PoseStamped to_frame(const geometry_msgs::msg::PoseStamped::SharedPtr point_ptr, std::string frame_id);
 
     rclcpp_action::GoalResponse handle_goal(
        const rclcpp_action::GoalUUID & uuid,
--- motor_move/src/motor_move.cpp
+++ motor_move/src/motor_move.cpp
@@ -17,11 +17,32 @@ namespace motor_move {
     }
     MotorMove::~MotorMove() {
     }
+
+    PoseStamped
+    MotorMove::to_frame(const PoseStamped::SharedPtr point_ptr, std::string frame_id) {
+        PoseStamped point_out;
+        try {
+            tf_buffer_->transform(*point_ptr, point_out, frame_id);
+            return point_out;
+        } catch (const tf2::TransformException & ex) {
+            RCLCPP_WARN(this->get_logger(), "Failure %s\n", ex.what());
+            throw ex;
+        }
+    }
     rclcpp_action::GoalResponse MotorMove::handle_goal(
         const rclcpp_action::GoalUUID & uuid,
         std::shared_ptr<const MotorMoveAction::Goal> goal) {
         (void)uuid;
-        RCLCPP_INFO(this->get_logger(), "Received goal requst with order %f", goal->motor_goal.pose.position.x);
+        RCLCPP_INFO(this->get_logger(), "Received goal requst with order x: %f y: %f",
+                    goal->motor_goal.pose.position.x, goal->motor_goal.pose.position.y);
+        try {
+            std::lock_guard lock{target_pose_mutex_};
+            target_pose_ = to_frame(std::make_shared<PoseStamped>(goal->motor_goal), "odom");
+        }
+        catch (tf2::TransformException & ex) {
+            RCLCPP_ERROR(this->get_logger(), "Transform error: %s", ex.what());
+            return rclcpp_action::GoalResponse::REJECT;
+        }
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
@@ -41,7 +62,10 @@ namespace motor_move {
         (void)goal_handle;
         rclcpp::Rate loop_rate(15);
         RCLCPP_INFO(this->get_logger(), "Execute goal");
+        PoseStamped error = to_frame(std::make_shared<PoseStamped>(target_pose_), "base_link");
         loop_rate.sleep();
+        RCLCPP_INFO(this->get_logger(), "Delta x: %f y: %f",
+                    error.pose.position.x, error.pose.position.y);
     }
 }
 int main(int argc, char **argv) {
