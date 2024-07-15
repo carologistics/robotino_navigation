e2f9d2bac11790269490cb79a7e047f12093607d
saurabh borse <saurabh.borse@alumni.fh-aachen.de>
2024-07-15 16:09:47 +0200

    mapfplanner: new baseline for mapf bringup launch

---
+++ robotino_navigation/launch/mapf_bringup.launch.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+# Licensed under MIT. See LICENSE file. Copyright Carologistics.
+import os
+import sys
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import GroupAction
+from launch.actions import IncludeLaunchDescription
+from launch.actions import OpaqueFunction
+from launch.actions import SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from robotino_utils import find_file
+
+
+def launch_nodes_withconfig(context, *args, **kwargs):
+
+    # Get the launch directory
+    bringup_dir = get_package_share_directory("robotino_navigation")
+    launch_dir = os.path.join(bringup_dir, "launch")
+
+    # launch configuration:common
+    namespace = LaunchConfiguration("namespace")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    autostart = LaunchConfiguration("autostart")
+    use_respawn = LaunchConfiguration("use_respawn")
+    # Launch configuration:mapf_centralagent
+    input_map_yaml_file = LaunchConfiguration("map")
+    launch_mapserver = LaunchConfiguration("launch_mapserver")
+    launch_plannerserver = LaunchConfiguration("launch_plannerserver")
+    mapf_centralagent_param_file = LaunchConfiguration("mapf_centralagent_param_file")
+    # Launch configuration:navigation
+    launch_mps_map_gen = LaunchConfiguration("launch_mps_map_gen")
+    launch_nav2rviz = LaunchConfiguration("launch_nav2rviz")
+    rviz_config = LaunchConfiguration("rviz_config")
+    input_params_file = LaunchConfiguration("params_file")
+    input_host_params_file = LaunchConfiguration("host_params_file")
+    team_name = LaunchConfiguration("team_name")
+
+    launch_configuration = {}
+    for argname, argval in context.launch_configurations.items():
+        launch_configuration[argname] = argval
+
+    map_yaml_file = find_file(input_map_yaml_file.perform(context), [bringup_dir + "/map/"])
+    if map_yaml_file is None:
+        print("Can not find %s, abort!", input_map_yaml_file.perform(context))
+        sys.exit(1)
+    params_file = find_file(input_params_file.perform(context), [bringup_dir + "/config/"])
+    if params_file is None:
+        print("Can not find %s, abort!", input_params_file.perform(context))
+        sys.exit(1)
+    host_params_file = find_file(input_host_params_file.perform(context), [bringup_dir + "/config/"])
+    if host_params_file is None:
+        print("Can not find %s, abort!", input_host_params_file.perform(context))
+        sys.exit(1)
+
+    launch_mps_map_gen_value = launch_mps_map_gen.perform(context).lower() in ["true", "1", "t", "y", "yes"]
+    if launch_mps_map_gen_value:
+        mps_map_gen_dir = get_package_share_directory("mps_map_gen")
+
+    # Specify the actions
+    actions = [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir, "mapf_centralagent.launch.py")),
+            launch_arguments={
+                "namespace": namespace,
+                "map": map_yaml_file,
+                "use_sim_time": use_sim_time,
+                "autostart": autostart,
+                "params_file": mapf_centralagent_param_file,
+                "use_respawn": use_respawn,
+                "launch_mapserver": launch_mapserver,
+                "launch_plannerserver": launch_plannerserver,
+            }.items(),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir, "mapf_navigation.launch.py")),
+            launch_arguments={
+                "namespace": namespace,
+                "use_sim_time": use_sim_time,
+                "autostart": autostart,
+                "params_file": params_file,
+                "host_params_file": host_params_file,
+                "use_respawn": use_respawn,
+            }.items(),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir, "robotino_rviz.launch.py")),
+            launch_arguments={
+                "namespace": namespace,
+                "launch_rviz": launch_nav2rviz,
+                "rviz_config": rviz_config,
+            }.items(),
+        ),
+    ]
+    if launch_mps_map_gen_value:
+        actions.extend(
+            [
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(os.path.join(mps_map_gen_dir, "mps_map_gen.launch.py")),
+                    launch_arguments={
+                        "namespace": namespace,
+                        "use_sim_time": use_sim_time,
+                        "get_data_from_refbox": "true",
+                        "publish_wait_pos": "true",
+                        "peer_address": "172.26.255.255",
+                        "team_name": team_name,
+                        "recv_port_cyan": "4441",
+                        "recv_port_magenta": "4442",
+                        "recv_port_public": "4444",
+                        "crypto_key": "randomkey",
+                        "map_client": "/map_server/map",
+                    }.items(),
+                )
+            ]
+        )
+    bringup_cmd_group = GroupAction(actions)
+
+    return [bringup_cmd_group]
+
+
+def generate_launch_description():
+    # Get the launch directory
+    package_dir = get_package_share_directory("robotino_navigation")
+
+    # Declare the launch arguments
+    stdout_linebuf_envvar = SetEnvironmentVariable("RCUTILS_LOGGING_BUFFERED_STREAM", "1")
+
+    declare_namespace_cmd = DeclareLaunchArgument("namespace", default_value="", description="Top-level namespace")
+
+    declare_launch_rviz_cmd = DeclareLaunchArgument(
+        "launch_rviz",
+        default_value="false",
+        description="Weather to launch rviz or not",
+    )
+
+    declare_launch_mps_map_gen_cmd = DeclareLaunchArgument(
+        "launch_mps_map_gen",
+        default_value="false",
+        description="Weather to launch mps_map_gen or not",
+    )
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        "map",
+        default_value=os.path.join(package_dir, "map", "map_sf_sim.yaml"),
+        description="Full path to map yaml file to load",
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=[os.path.join(package_dir, "config/"), "nav2_params.yaml"],
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
+    )
+
+    declare_host_params_file_cmd = DeclareLaunchArgument(
+        "host_params_file",
+        default_value=[
+            os.path.join(package_dir, "config/"),
+            LaunchConfiguration("namespace"),
+            "_nav2_params",
+            ".yaml",
+        ],
+        description="Full path to the host-specific ROS2 parameters file to use for all launched nodes",
+    )
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart",
+        default_value="true",
+        description="Automatically startup the nav2 stack",
+    )
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        "use_respawn",
+        default_value="False",
+        description="Whether to respawn if a node crashes. Applied when composition is disabled.",
+    )
+
+    declare_log_level_cmd = DeclareLaunchArgument("log_level", default_value="info", description="log level")
+
+    declare_launchmapserver_cmd = DeclareLaunchArgument(
+        "launch_mapserver",
+        default_value="true",
+        description="whether to launch map server or not",
+    )
+
+    declare_launchplannerserver_cmd = DeclareLaunchArgument(
+        "launch_plannerserver",
+        default_value="true",
+        description="whether to launch map server or not",
+    )
+
+    declare_launch_nav2rviz_cmd = DeclareLaunchArgument(
+        "launch_nav2rviz",
+        default_value="false",
+        description="whether to launch rviz or not",
+    )
+
+    declare_rvizconfig_cmd = DeclareLaunchArgument(
+        "rviz_config",
+        default_value=[os.path.join(package_dir, "rviz/"), "nav2config.rviz"],
+        description="Full path to the RVIZ config file to use for all launched nodes",
+    )
+
+    declare_team_name_cmd = DeclareLaunchArgument(
+        "team_name",
+        default_value="Carologistics",
+        description="Which team name registered for the RefBox (important for mps_map_gen)",
+    )
+
+    declare_mapfparams_file_cmd = DeclareLaunchArgument(
+        "mapf_centralagent_param_file",
+        default_value=[os.path.join(package_dir, "config/"), "mapf_centralagent.yaml"],
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Set environment variables
+    ld.add_action(stdout_linebuf_envvar)
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_launch_rviz_cmd)
+    ld.add_action(declare_launch_mps_map_gen_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_host_params_file_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_log_level_cmd)
+    ld.add_action(declare_launchmapserver_cmd)
+    ld.add_action(declare_launch_nav2rviz_cmd)
+    ld.add_action(declare_rvizconfig_cmd)
+    ld.add_action(declare_team_name_cmd)
+    ld.add_action(declare_mapfparams_file_cmd)
+    ld.add_action(declare_launchplannerserver_cmd)
+
+    # Add the actions to launch all of the navigation nodes
+    ld.add_action(OpaqueFunction(function=launch_nodes_withconfig))
+
+    return ld
---
+++ robotino_navigation/launch/mapf_centralagent.launch.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+# Licensed under MIT. See LICENSE file. Copyright Carologistics.
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import GroupAction
+from launch.actions import OpaqueFunction
+from launch.actions import SetEnvironmentVariable
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml
+
+
+def launch_nodes_withconfig(context, *args, **kwargs):
+
+    bringup_dir = get_package_share_directory("robotino_navigation")
+
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration("namespace")
+    map_yaml_file = LaunchConfiguration("map")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    autostart = LaunchConfiguration("autostart")
+    params_file = LaunchConfiguration("params_file")
+    use_respawn = LaunchConfiguration("use_respawn")
+    log_level = LaunchConfiguration("log_level")
+    launch_mapserver = LaunchConfiguration("launch_mapserver")
+    launch_plannerserver = LaunchConfiguration("launch_plannerserver")
+    num_of_agents = LaunchConfiguration("num_of_agents")
+
+    lifecycle_nodes = ["mapf_map_server"]
+    if launch_plannerserver:
+        lifecycle_nodes.append("mapf_planner_server")
+
+    launch_configuration = {}
+    for argname, argval in context.launch_configurations.items():
+        launch_configuration[argname] = argval
+
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {"yaml_filename": map_yaml_file, "number_of_agents": num_of_agents}
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
+    # Create the remappings for the nodes
+    remappings = [
+        ("/" + launch_configuration["namespace"] + "/tf", "/tf"),
+        ("/" + launch_configuration["namespace"] + "/tf_static", "/tf_static"),
+        ("/" + launch_configuration["namespace"] + "/map", "/map"),
+    ]
+
+    os.path.join(bringup_dir, "rviz", "robotino_localization.rviz")
+
+    # Create list of nodes to launch
+    load_nodes = GroupAction(
+        actions=[
+            Node(
+                package="nav2_map_server",
+                executable="map_server",
+                name="mapf_map_server",
+                output="screen",
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=["--ros-args", "--log-level", log_level],
+                remappings=remappings,
+                condition=IfCondition(launch_mapserver),
+                namespace=namespace,
+            ),
+            Node(
+                package="nav2_mapf_planner",
+                executable="mapf_planner_server",
+                name="mapf_planner_server",
+                output="screen",
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=["--ros-args", "--log-level", log_level],
+                remappings=remappings,
+                condition=IfCondition(launch_plannerserver),
+                namespace=namespace,
+            ),
+            Node(
+                package="nav2_lifecycle_manager",
+                executable="lifecycle_manager",
+                name="lifecycle_manager_centralagent",
+                output="screen",
+                arguments=["--ros-args", "--log-level", log_level],
+                parameters=[
+                    {"use_sim_time": use_sim_time},
+                    {"autostart": autostart},
+                    {"node_names": lifecycle_nodes},
+                ],
+                namespace=namespace,
+            ),
+        ]
+    )
+
+    return [load_nodes]
+
+
+def generate_launch_description():
+
+    package_dir = get_package_share_directory("robotino_navigation")
+
+    # Declare the launch arguments
+    stdout_linebuf_envvar = SetEnvironmentVariable("RCUTILS_LOGGING_BUFFERED_STREAM", "1")
+
+    declare_namespace_cmd = DeclareLaunchArgument("namespace", default_value="", description="Top-level namespace")
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        "map",
+        default_value=os.path.join(package_dir, "map", "map_sf_sim.yaml"),
+        description="Full path to map yaml file to load",
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart",
+        default_value="true",
+        description="Automatically startup the nav2 stack",
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=[os.path.join(package_dir, "config/"), "mapf_centralagent.yaml"],
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
+    )
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        "use_respawn",
+        default_value="true",
+        description="Whether to respawn if a node crashes. Applied when composition is disabled.",
+    )
+
+    declare_log_level_cmd = DeclareLaunchArgument("log_level", default_value="info", description="log level")
+
+    declare_launch_mapserver_cmd = DeclareLaunchArgument(
+        "launch_mapserver",
+        default_value="true",
+        description="Wheather to start Rvizor not based on launch environment",
+    )
+
+    declare_launch_plannerserver_cmd = DeclareLaunchArgument(
+        "launch_plannerserver",
+        default_value="true",
+        description="Wheather to start Rvizor not based on launch environment",
+    )
+
+    declare_launch_plannerserver_cmd = DeclareLaunchArgument(
+        "num_of_agents",
+        default_value="1",
+        description="Wheather to start Rvizor not based on launch environment",
+    )
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Set environment variables
+    ld.add_action(stdout_linebuf_envvar)
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_log_level_cmd)
+    ld.add_action(declare_launch_mapserver_cmd)
+    ld.add_action(declare_launch_plannerserver_cmd)
+
+    # Add the actions to launch all of the localiztion nodes
+    ld.add_action(OpaqueFunction(function=launch_nodes_withconfig))
+
+    return ld
