928fb12821b21bda8c6719c6eee10b96cdebbaa5
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2024-07-15 15:11:41 +0200

    map: add RC2024 map

--- robotino_navigation/launch/robotino_bringup.launch.py
+++ robotino_navigation/launch/robotino_bringup.launch.py
@@ -158,7 +158,7 @@ def generate_launch_description():
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         "map",
-        default_value=os.path.join(package_dir, "map", "map_sf_empty.yaml"),
+        default_value=os.path.join(package_dir, "map", "RC2024_map.yaml"),
         description="Full path to map yaml file to load",
     )
 
