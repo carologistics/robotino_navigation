ba270d1e147d42635d1897ae14dcf586352748eb
Zhen Yan Khaw <zhen-yan.khaw@alumni.fh-aachen.de>
2025-04-07 17:01:37 +0200

    Navigation : add a aruco tag tracker

---
+++ aruco_tracker/launch/aruco.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=[
+                '--x', '-0.52', '--y', '0', '--z', '0',
+                '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'base_link', '--child-frame-id', 'camera_link']
+        ),
+        Node(
+             package='aruco_tracker',
+             executable='aruco',
+             name='aruco',
+         ),
+    ])
