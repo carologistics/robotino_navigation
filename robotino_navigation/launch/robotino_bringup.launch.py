11422b441b8c8b647af7a9dc4120d6cb45141735
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2025-07-09 12:42:38 +0200

    map: add temporary map for sf while packing

--- robotino_navigation/launch/robotino_bringup.launch.py
+++ robotino_navigation/launch/robotino_bringup.launch.py
@@ -158,7 +158,7 @@ def generate_launch_description():
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         "map",
-        default_value=os.path.join(package_dir, "map", "map_sf_empty.yaml"),
+        default_value=os.path.join(package_dir, "map", "map_sf_pre_rc2025.yaml"),
         description="Full path to map yaml file to load",
     )
 
